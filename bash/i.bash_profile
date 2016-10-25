c1aac7cdef8156a111d424c4956f1286309e1c31
jerry <jerryzhou@outlook.com>
2016-10-26 00:02:10 +0800

    fix the missing dir

--- bash/i.bash_profile
+++ bash/i.bash_profile
@@ -87,6 +87,6 @@ if [ -f $(brew --prefix)/etc/bash_completion ]; then
     . $(brew --prefix)/etc/bash_completion
 fi
 
-export GOPATH=~/selfgo
-export GOBIN=~/selfgo/bin
+export GOPATH=~/Documents/selfgo
+export GOBIN=~/Documents/selfgo/bin
 
--- prepare.sh
+++ prepare.sh
@@ -2,7 +2,7 @@ git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
 brew install bash-completion
 
-mkdir ~/selfgo
-mkdir ~/selfgo/bin
-mkdir ~/selfgo/src
-mkdir ~/selfgo/pkg
+mkdir ~/Documents/selfgo
+mkdir ~/Documents/selfgo/bin
+mkdir ~/Documents/selfgo/src
+mkdir ~/Documents/selfgo/pkg
